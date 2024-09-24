5c0b3d15927e04cfacfd1ff59c016747b4ac87b9
Grazielle <grazielleconceicao@yahoo.com.br>
2024-09-24 08:39:41 -0300

    feat: Orders list integreated with API

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,10 +1,23 @@
 import { Button } from "@/components/ui/button";
-import { Dialog,  DialogTrigger } from "@/components/ui/dialog";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import { TableCell, TableRow } from "@/components/ui/table";
 import { ArrowRight, Search, X } from "lucide-react";
 import { OrderDetails } from "./order-details";
+import { OrderStatus } from "@/components/order-status";
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 
-export function OrderTableRow() {
+export interface OrderTableRowProps {
+    order: {
+        orderId: string;
+        createdAt: string;
+        status: "pending" | "canceled" | "processing" | "delivering" | "delivered";
+        customerName: string;
+        total: number;
+    }
+}
+
+export function OrderTableRow({ order }: OrderTableRowProps) {
     return (
         <TableRow>
             <TableCell>
@@ -20,19 +33,16 @@ export function OrderTableRow() {
                 </Dialog>
 
             </TableCell>
-            <TableCell className="font-mono text-xs font-medium">345353fggdfg</TableCell>
+            <TableCell className="font-mono text-xs font-medium">{order.orderId}</TableCell>
             <TableCell className="text-muted-foreground">
-                há 15 minutos
+                {formatDistanceToNow(order.createdAt, { locale: ptBR, addSuffix: true })}
             </TableCell>
             <TableCell>
-                <div className="flex items-center gap-2">
-                    <span className="h-2 w-2 rounded-full bg-slate-400" />
-                    <span className="font-medium text-muted-foreground">Pendente</span>
-                </div>
+                <OrderStatus status={order.status} />
             </TableCell>
-            <TableCell className="font-medium">Grazielle Conceição</TableCell>
+            <TableCell className="font-medium">{order.customerName}</TableCell>
             <TableCell className="font-medium">
-                R$ 149,90
+                {order.total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
             </TableCell>
             <TableCell>
                 <Button variant="outline" size="xs">
